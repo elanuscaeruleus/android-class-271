d48532810d99e8728e94d4ff51e6debef480f243
elanuscaeruleus <elanusfly@gmail.com>
2016-07-26 12:23:48 +0800

    hw1

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -2,12 +2,60 @@ package com.example.user.simpleui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    EditText editText;
+    RadioGroup radioGroup;
+
+    String sex="male";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        editText = (EditText)findViewById(R.id.editText);
+        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId==R.id.maleRadioButton)
+                {
+                    sex="male";
+                }
+                else if(checkedId==R.id.femaleRadioButton2)
+                {
+                    sex="female";
+                }
+        }
+    });
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+                      public boolean onKey(View v, int keyCode, KeyEvent event) {
+                              if(keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                                    submit(v);
+                                    return true;
+                              }
+                                   return false;
+                           }
+              });
+
+    }
+    public void submit(View view)
+    {
+        textView.setText("Hello Android");
+        String text=editText.getText().toString();
+        text=text+"性別:"+sex;
+        textView.setText(text);
+        editText.setText("");
     }
 }
