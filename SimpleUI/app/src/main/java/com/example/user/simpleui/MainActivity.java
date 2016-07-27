5c46add44a0d80b1d2b7819251bb6e879f8ad32b
elanuscaeruleus <elanusfly@gmail.com>
2016-07-27 12:32:50 +0800

    add Snacjbar

--- SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.user.simpleui;
 
+import android.net.Uri;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -11,6 +14,11 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,28 +30,33 @@ public class MainActivity extends AppCompatActivity {
     ListView listView;
     Spinner spinner;
 
-    String drink="black tea";
+    String drink = "black tea";
 
-    List<Order> orders=new ArrayList<>();
+    List<Order> orders = new ArrayList<>();
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView)findViewById(R.id.textView);
-        editText = (EditText)findViewById(R.id.editText);
-        radioGroup=(RadioGroup)findViewById(R.id.radioGroup);
-        listView=(ListView)findViewById(R.id.listView);
-        spinner=(Spinner)findViewById(R.id.spinner);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        listView = (ListView) findViewById(R.id.listView);
+        spinner = (Spinner) findViewById(R.id.spinner);
 
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton=(RadioButton)group.findViewById(checkedId);
-                drink=radioButton.getText().toString();
-        }
-    });
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+                drink = radioButton.getText().toString();
+            }
+        });
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -55,38 +68,93 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-              setupListView();
-              setupSpinner();
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Order order=(Order)parent.getAdapter().getItem(position);
+//                Toast.makeText(MainActivity.this, "You click on"+order.note, Toast.LENGTH_SHORT).show();
+                Snackbar.make(parent, "You click on" + order.note, Snackbar.LENGTH_SHORT).setAction("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        
+                    }
+                })show();
+            }
+        });
+        setupListView();
+        setupSpinner();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    public void setupListView()
-    {
+
+    public void setupListView() {
 //        String[] data=new String[]{"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"};
 //        ArrayAdapter adapter= new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, orders);
-        OrderAdapter adapter=new OrderAdapter(this, orders);
+        OrderAdapter adapter = new OrderAdapter(this, orders);
         listView.setAdapter(adapter);
     }
 
-    public void setupSpinner()
-    {
-        String[] data=getResources().getStringArray(R.array.storeInfos);
-        ArrayAdapter adapter= new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item, data);
+    public void setupSpinner() {
+        String[] data = getResources().getStringArray(R.array.storeInfos);
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, data);
         spinner.setAdapter(adapter);
     }
 
-    public void submit(View view)
-    {
-        String text=editText.getText().toString();
-        String result=text+" order:"+drink;
+    public void submit(View view) {
+        String text = editText.getText().toString();
+        String result = text + " order:" + drink;
         textView.setText(result);
         editText.setText("");
 
-        Order order=new Order();
-        order.note=text;
-        order.drink=drink;
-        order.storeInfo=(String)spinner.getSelectedItem();
+        Order order = new Order();
+        order.note = text;
+        order.drink = drink;
+        order.storeInfo = (String) spinner.getSelectedItem();
 
         orders.add(order);
         setupListView();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.user.simpleui/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.user.simpleui/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
