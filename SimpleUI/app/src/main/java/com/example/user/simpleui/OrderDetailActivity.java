da12cfd8af4981ba6a134b9775d85ac322dea5d0
elanuscaeruleus <elanusfly@gmail.com>
2016-08-04 12:24:53 +0800

    implement asynctask to download static map

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,14 +1,19 @@
 package com.example.user.simpleui;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telecom.Call;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.os.Handler;
+
+import java.lang.ref.WeakReference;
 import java.util.logging.LogRecord;
 
 public class OrderDetailActivity extends AppCompatActivity {
@@ -20,11 +25,11 @@ public class OrderDetailActivity extends AppCompatActivity {
 
         Order order=getIntent().getParcelableExtra("order");
 
-        TextView notetextView=(TextView)findViewById(R.id.noteTextView);
+        TextView NotetextView=(TextView)findViewById(R.id.NoteTextView);
         TextView orderResultTextView=(TextView)findViewById(R.id.orderResultTextView);
         TextView storeInfoTextView=(TextView)findViewById(R.id.storeInfoTextView);
 
-        notetextView.setText(order.getNote());
+        NotetextView.setText(order.getNote());
         storeInfoTextView.setText(order.getStoreInfo());
 
         String orderResultText="";
@@ -37,7 +42,10 @@ public class OrderDetailActivity extends AppCompatActivity {
         }
         orderResultTextView.setText(orderResultText);
 
-        final TextView testTextView=(TextView)findViewById(R.id.testTextView);
+        ImageView staticImageView=(ImageView)findViewById(R.id.staticMapImageView);
+
+        (new GeoCodingTask(staticImageView)).execute("台北市羅斯福路四段一號");
+        Log.e("Main Thread ID", Long.toString(Thread.currentThread().getId()));
 
        /* final Handler handler=new Handler(new Handler.Callback() {
             @Override
@@ -70,5 +78,37 @@ public class OrderDetailActivity extends AppCompatActivity {
 
         thread.start();*/
         Log.e("Main Thread ID", Long.toString(Thread.currentThread().getId()));
+    }
+
+    public static class GeoCodingTask extends AsyncTask<String, Void, Bitmap>{
+
+        WeakReference<ImageView> imageViewWeakReference;
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            double[] latlng=Utils.getLatLngFromAddress(params[0]);
+            if(latlng!=null)
+            {
+                return Utils.getStaticMapFromLatLng(latlng);
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if(bitmap!=null)
+            {
+                if(imageViewWeakReference.get()!=null)
+                {
+                    ImageView imageView=imageViewWeakReference.get();
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+        }
+        public GeoCodingTask(ImageView imageView)
+        {
+            imageViewWeakReference=new WeakReference<ImageView>(imageView);
+        }
     }
 }
